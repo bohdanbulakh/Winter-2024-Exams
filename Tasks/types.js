b3c325d94f4278d21f140608880c00753d0a0ec1
bohdan <bgdn.56t@gmail.com>
2024-01-14 15:39:05 +0200

    updated types.js: changed function expression to arrow function

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,7 +1,7 @@
 // Count types in an array
 'use strict';
 
-types_ = function (s) {
+const types = (s) => {
   types_ = {
     number: 0,
     string: 0,
@@ -16,4 +16,4 @@ types_ = function (s) {
   s.length;
 };
 
-module.exports = types_;
+module.exports = types;
