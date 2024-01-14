e0c7070a3d0cbf06fd144d3119de72d18159d4b2
bohdan <bgdn.56t@gmail.com>
2024-01-14 13:21:10 +0200

    updated filter.js: fixed arrow function and variables declaration

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,15 @@
 // Filter array by type name
 'use strict';
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
+const Filter = (T, t) => {
+  const remove = [];
+  for (const C of T) {
+    const x = T.indexOf(C);
     if (typeof T[x] !== t) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
+  for (const x of remove) T.splice(x, 1);
   return T;
 };
 
