72aff86526558179462c3bfb0a53798c9dfd0aca
bohdan <bgdn.56t@gmail.com>
2024-01-14 08:06:19 +0200

    updated skip.js: removed unused x variable

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -3,14 +3,12 @@
 
 const Skip = (T, ...Remove) => {
   const result = [];
-  x = 0;
   for (const C of T) {
     for (const X of Remove) {
       if (C !== X) {
         result.push(C);
       }
     }
-    x++;
   }
   return result;
 };
