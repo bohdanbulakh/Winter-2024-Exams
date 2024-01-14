f8ddf12094e306c9b78256232f3865288f4da06b
bohdan <bgdn.56t@gmail.com>
2024-01-14 13:40:33 +0200

    updated filter.js: added result variable, simplified filter algorithm

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,15 +2,14 @@
 'use strict';
 
 const Filter = (T, t) => {
-  const remove = [];
+  const result = [];
   for (const C of T) {
     const x = T.indexOf(C);
-    if (typeof T[x] !== t) {
-      remove.unshift(x);
+    if (typeof T[x] === t) {
+      result.push(C);
     }
   }
-  for (const x of remove) T.splice(x, 1);
-  return T;
+  return result;
 };
 
 module.exports = Filter;
