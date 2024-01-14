325a95ab8f7f98d6314c87fa6c4dbc58f050f4c6
bohdan <bgdn.56t@gmail.com>
2024-01-14 14:46:29 +0200

    updated filter.js: improved naming of identifiers

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,12 +1,12 @@
 // Filter array by type name
 'use strict';
 
-const Filter = (T, t) => {
+const filterByType = (array, typeName) => {
   const result = [];
-  for (const C of T) {
-    if (typeof C === t) result.push(C);
+  for (const element of array) {
+    if (typeof element === typeName) result.push(element);
   }
   return result;
 };
 
-module.exports = Filter;
+module.exports = filterByType;
