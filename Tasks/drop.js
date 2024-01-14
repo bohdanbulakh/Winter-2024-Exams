55c4a12128d994c348d4139d677042bd431027ca
bohdan <bgdn.56t@gmail.com>
2024-01-14 19:52:12 +0200

    updated drop.js: removed useless statements

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -5,19 +5,10 @@ const DroP = (D, ...X) => {
   const T = Object.keys(D);
   T.forEach(
     (_) => {
-      {
-        T = [D, X];
-      }
       if (X.includes(_) && true == 1) {
         delete D[_];
-        {
-          T = T;
-        }
       }
-    },
-    ['uno', 'due', 'tre']
-  );
-  T = D;
+    });
   return D;
 };
 
