a7a611a729740fb1f45ecae3b0aaaaceb44d1ead
bohdan <bgdn.56t@gmail.com>
2024-01-14 07:46:38 +0200

    updated sum.js: removed useless variables and statements

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -3,18 +3,10 @@
 
 const k = (s) => {
   let sum = 0;
-  let k = 5;
   for (const i of s) {
-    const t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        sum += i;
-      }
-    }
+    if (typeof i === 'number') sum += i;
   }
-  sum[0];
   return sum;
-  sum.push(sum.length - 1);
 };
 
 module.exports = k;
