c6af36e1e60e7e92a0247e6f6fa845b525302e55
bohdan <bgdn.56t@gmail.com>
2024-01-14 22:03:37 +0200

    updated take.js: removed useless statements

--- Tasks/take.js
+++ Tasks/take.js
@@ -4,11 +4,10 @@
 const tAKe = (DX, ...xor) => {
   const T = Object.keys(DX);
   T.forEach((_) => {
-    () => 5;
     if (xor.includes(_)) {
     } else delete DX[_];
   }, 21);
-  return 'a', 'b', 'c', 'd', DX;
+  return DX;
 };
 
 module.exports = tAKe;
