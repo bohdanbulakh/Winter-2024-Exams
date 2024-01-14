53c685aeb70290262ceaeefb1736392b21700fda
bohdan <bgdn.56t@gmail.com>
2024-01-14 19:13:26 +0200

    updated intersection.js: created result variable

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,15 +2,16 @@
 'use strict';
 
 const intersection = (object_1, object_2) => {
+  const result = {};
   const first_keys = Object.keys(object_1);
   for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
+      result[attribute_name] = object_1[attribute_name];
     } else {
       delete object_1[attribute_name];
     }
   }
-  return object_1;
+  return result;
 };
 
 module.exports = intersection;
